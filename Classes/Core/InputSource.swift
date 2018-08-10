291b7e2756f875746595326d38bf64be2d85da96
Faisal <faisal.azeez@msisoft.in>
2018-08-10 11:21:59 +0530

    Change titles and styling on homepage

---
+++ Classes/Core/InputSource.swift
@@ -0,0 +1,55 @@
+//
+//  InputSource.swift
+//  ImageSlideshow
+//
+//  Created by Petr Zvoníček on 14.01.16.
+//
+//
+
+import UIKit
+
+/// A protocol that can be adapted by different Input Source providers
+@objc public protocol InputSource {
+    /**
+     Load image from the source to image view.
+     - parameter imageView: Image view to load the image into.
+     - parameter callback: Callback called after image was set to the image view.
+     - parameter image: Image that was set to the image view.
+     */
+    func load(to imageView: UIImageView, with callback: @escaping (_ image: UIImage?) -> Void)
+    
+    @objc optional func loadWithProgress(to imageView: UIImageView, with callback: @escaping (_ image: UIImage?) -> Void, progress: @escaping(_ progress: CGFloat) ->Void)
+    /**
+     Cancel image load on the image view
+     - parameter imageView: Image view that is loading the image
+    */
+    @objc optional func cancelLoad(on imageView: UIImageView)
+}
+
+/// Input Source to load plain UIImage
+@objcMembers
+open class ImageSource: NSObject, InputSource {
+    var image: UIImage!
+
+    /// Initializes a new Image Source with UIImage
+    /// - parameter image: Image to be loaded
+    public init(image: UIImage) {
+        self.image = image
+    }
+
+    /// Initializes a new Image Source with an image name from the main bundle
+    /// - parameter imageString: name of the file in the application's main bundle
+    public init?(imageString: String) {
+        if let image = UIImage(named: imageString) {
+            self.image = image
+            super.init()
+        } else {
+            return nil
+        }
+    }
+
+    public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
+        imageView.image = image
+        callback(image)
+    }
+}
---
+++ Classes/InputSources/KingfisherSource.swift
@@ -0,0 +1,67 @@
+//
+//  KingfisherSource.swift
+//  ImageSlideshow
+//
+//  Created by feiin
+//
+//
+
+import Kingfisher
+
+/// Input Source to image using Kingfisher
+public class KingfisherSource: NSObject, InputSource {
+    /// url to load
+    public var url: URL
+
+    /// placeholder used before image is loaded
+    public var placeholder: UIImage?
+    
+    /// options for displaying, ie. [.transition(.fade(0.2))]
+    public var options: KingfisherOptionsInfo?
+
+    /// Initializes a new source with a URL
+    /// - parameter url: a url to be loaded
+    /// - parameter placeholder: a placeholder used before image is loaded
+    /// - parameter options: options for displaying
+    public init(url: URL, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
+        self.url = url
+        self.placeholder = placeholder
+        self.options = options
+        super.init()
+    }
+
+    /// Initializes a new source with a URL string
+    /// - parameter urlString: a string url to load
+    /// - parameter placeholder: a placeholder used before image is loaded
+    /// - parameter options: options for displaying
+    public init?(urlString: String, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
+        if let validUrl = URL(string: urlString) {
+            self.url = validUrl
+            self.placeholder = placeholder
+            self.options = options
+            super.init()
+        } else {
+            return nil
+        }
+    }
+
+    @objc public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
+        imageView.kf.setImage(with: self.url, placeholder: self.placeholder, options: self.options, progressBlock: nil) { (image, _, _, _) in
+            callback(image)
+        }
+    }
+    
+    public func loadWithProgress(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void, progress: @escaping (CGFloat) -> Void) {
+        imageView.kf.setImage(with: self.url, placeholder: self.placeholder, options: self.options, progressBlock:{
+            (recieved,total)
+            in
+            progress((CGFloat(recieved)/CGFloat(total)))
+        }) { (image, _, _, _) in
+            callback(image)
+        }
+    }
+    
+    public func cancelLoad(on imageView: UIImageView) {
+        imageView.kf.cancelDownloadTask()
+    }
+}
